071f2cf6c8d2e06e2f23ff64f5791cf400fcc67b
Anton Kuts <anton.a.kuts@gmail.com>
2024-03-13 22:15:35 +0200

    practiced with route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import styles from "./page.module.css";
 
 const HomePage = () => {
 	return (
 		<main className={styles.main}>
 			<h1>Hello World</h1>
+
+			<Link href="/properties">Click me</Link>
 		</main>
 	);
 }
---
+++ src/app/properties/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import Link from "next/link"
+
+const ProportiesPage = () => {
+	
+	return (
+		<>
+			<h1>Properties Page</h1>
+			<Link href='/'>Go Home</Link>
+		</>
+	)
+}
+
+export default ProportiesPage
